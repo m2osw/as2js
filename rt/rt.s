cf38c0eb8786697d283fc6c3b576759a614a4d1b
AlexisWilke <alexis@m2osw.com>
2023-03-09 07:52:39 -0800

    Fixed the rt_power() function.
    Now the power function is fully tested and works as expected in all cases.
    The code runs as expected and I added an rt library (for when I write the
    output to C++ instead of binary).

---
+++ rt/rt.s
@@ -0,0 +1,24 @@
+// Copyright (c) 2011-2023  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/as2js
+// contact@m2osw.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+.include    "rt/rt_power.rt"
+
+
+// vim: ts=4 sw=4 et
---
+++ tests/catch_rt.cpp
@@ -0,0 +1,160 @@
+// Copyright (c) 2011-2023  Made to Order Software Corp.  All Rights Reserved
+//
+// https://snapwebsites.org/project/as2js
+// contact@m2osw.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// as2js
+//
+#include    <rt/rt.h>
+
+//#include    <as2js/exception.h>
+//#include    <as2js/message.h>
+
+
+// self
+//
+#include    "catch_main.h"
+
+
+// snapdev
+//
+#include    <snapdev/math.h>
+//#include    <snapdev/ostream_to_buf.h>
+//#include    <snapdev/safe_stream.h>
+//#include    <snapdev/tokenize_string.h>
+
+
+// libutf8
+//
+//#include    <libutf8/iterator.h>
+
+
+// C++
+//
+//#include    <cstring>
+//#include    <algorithm>
+//#include    <iomanip>
+
+
+// C
+//
+//#include    <signal.h>
+//#include    <sys/wait.h>
+
+
+// last include
+//
+#include    <snapdev/poison.h>
+
+
+
+
+
+
+
+CATCH_TEST_CASE("rt_power", "[rt][math]")
+{
+    CATCH_START_SECTION("rt_power: n^0 = 1")
+    {
+        for(std::int64_t count(0); count < 100; ++count)
+        {
+            std::int64_t number(0);
+            SNAP_CATCH2_NAMESPACE::random(number);
+            CATCH_REQUIRE(rt_power(number, 0) == 1);
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("rt_power: n^1 = n")
+    {
+        for(std::int64_t count(0); count < 100; ++count)
+        {
+            std::int64_t number(0);
+            SNAP_CATCH2_NAMESPACE::random(number);
+            CATCH_REQUIRE(rt_power(number, 1) == number);
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("rt_power: n^-p = 0 (unless n = 1 or n = -1)")
+    {
+        for(std::int64_t p(-100); p < 0; ++p)
+        {
+            CATCH_REQUIRE(rt_power(1, p) == 1);
+            if((p & 1) == 0)
+            {
+                CATCH_REQUIRE(rt_power(-1, p) == 1);
+            }
+            else
+            {
+                CATCH_REQUIRE(rt_power(-1, p) == -1);
+            }
+        }
+
+        for(std::int64_t count(0); count < 100; ++count)
+        {
+            std::int64_t n(0);
+            SNAP_CATCH2_NAMESPACE::random(n);
+            std::int64_t p(0);
+            do
+            {
+                SNAP_CATCH2_NAMESPACE::random(p);
+                p |= 1ULL << 63;
+            }
+            while(p == -1 || p == 0);
+            CATCH_REQUIRE(rt_power(n, p) == 0);
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("rt_power: 2^p")
+    {
+        std::int64_t n(2);
+        for(std::int64_t p(1); p < 63; ++p)
+        {
+            CATCH_REQUIRE(rt_power(2, p) == n);
+            n *= 2;
+        }
+    }
+    CATCH_END_SECTION()
+
+    CATCH_START_SECTION("rt_power: n^p")
+    {
+        for(std::int64_t count(0); count < 100; ++count)
+        {
+            std::int64_t n(0);
+            SNAP_CATCH2_NAMESPACE::random(n);
+
+            // snapdev::pow() expects an `int` so we use such here, it will
+            // still be a very strong test
+            //
+            int p(0);
+            do
+            {
+                SNAP_CATCH2_NAMESPACE::random(p);
+            }
+            while(p < 2);
+
+//std::cerr << "n ^ p = " << n << " ^ " << p << "\n";
+            CATCH_REQUIRE(rt_power(n, p) == snapdev::pow(n, p));
+        }
+    }
+    CATCH_END_SECTION()
+}
+
+
+
+// vim: ts=4 sw=4 et
